f263e30705ff2b640d8eff386fb60cd8b9878a7d
niejun <jun.nie@icloud.com>
2021-03-24 18:19:11 +0800

    03月24号提交。

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     # 主页
     path('', views.index, name='index'),
     # 显示所有的主题。
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
+    # 特定主题的详细页面。
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # 用户添加新主题的页面。
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
